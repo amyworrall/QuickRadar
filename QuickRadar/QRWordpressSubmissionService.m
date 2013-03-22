4711188c24928fb3e242692436d67fe0768bce79
Oliver Drobnik <oliver@cocoanetics.com>
2013-03-22 18:32:52 +0100

    Added rdar and open radar links to wordpress post

--- QuickRadar/QRWordpressSubmissionService.m
+++ QuickRadar/QRWordpressSubmissionService.m
@@ -7,6 +7,8 @@
 //
 
 #import "QRWordpressSubmissionService.h"
+#import "NSError+Additions.h"
+#import "DTWordpress.h"
 
 @implementation QRWordpressSubmissionService
 
@@ -32,6 +34,29 @@
 
 + (BOOL)isAvailable
 {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *endpointURLString = [defaults stringForKey:@"WordpressURL"];
+    NSURL *endpointURL = [NSURL URLWithString:endpointURLString];
+    NSString *userName = [defaults stringForKey:@"WordpressUser"];
+    NSString *password = [defaults stringForKey:@"WordpressPassword"];
+    
+    NSString *scheme = [endpointURL scheme];
+    
+    if (!endpointURL || !([scheme isEqualToString:@"http"] || [scheme isEqualToString:@"https"]))
+    {
+        return NO;
+    }
+    
+    if (![userName length])
+    {
+        return NO;
+    }
+
+    if (![password length])
+    {
+        return NO;
+    }
+
     return YES;
 }
 
@@ -90,10 +115,69 @@
 {
     // TODO: implement wordpress submission here
     
-    dispatch_sync(dispatch_get_main_queue(), ^{
-        progressBlock();
-        completionBlock(YES, nil);
-    });
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSString *endpointURLString = [defaults stringForKey:@"WordpressURL"];
+    NSURL *endpointURL = [NSURL URLWithString:endpointURLString];
+    NSString *userName = [defaults stringForKey:@"WordpressUser"];
+    NSString *password = [defaults stringForKey:@"WordpressPassword"];
+
+        NSString *scheme = [endpointURL scheme];
+    
+    if (!endpointURL || !([scheme isEqualToString:@"http"] || [scheme isEqualToString:@"https"]))
+    {
+        return;
+    }
+    
+    if (![userName length])
+    {
+        return;
+    }
+    
+    if (![password length])
+    {
+        return;
+    }
+    
+    DTWordpress *wordpress = [[DTWordpress alloc] initWithEndpointURL:endpointURL];
+    wordpress.userName = userName;
+    wordpress.password = password;
+    
+    NSString *title = [NSString stringWithFormat:@"Radar: %@", self.radar.title];
+    NSMutableString *description = [NSMutableString string];
+    
+    [description appendFormat:@"Submitted as rdar://%ld", self.radar.radarNumber];
+    
+	if (self.radar.submittedToOpenRadar)
+	{
+        [description appendFormat:@" and to <a href=\"http://openradar.me/%ld\">OpenRadar</a>", self.radar.radarNumber];
+	}
+    
+    [description appendString:@".\n\n"];
+    
+    [description appendString:self.radar.body];
+    
+    NSDictionary *content = @{@"title":title, @"description":description, @"post_type":@"post"};
+    
+    [wordpress newPostWithContent:content shouldPublish:NO completion:^(NSInteger postID, NSError *error) {
+        if (error)
+        {
+            // wrap authentication error
+            if (error.code == 403)
+            {
+                error = [NSError authenticationErrorWithServiceIdentifier:QRWordpressSubmissionServiceIdentifier underlyingError:error];
+            }
+            
+            progressBlock();
+            completionBlock(NO, error);
+        }
+        else
+        {
+            NSLog(@"new post id %ld", (long)postID);
+            
+            progressBlock();
+            completionBlock(YES, nil);
+        }
+    }];
 }
 
 
