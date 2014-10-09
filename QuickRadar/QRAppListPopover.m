a77cb8720036aac49aab7794ba8c731be8197ca0
Rob Rix <rob.rix@github.com>
2014-10-09 01:52:01 -0400

    Correct the version check for the SDK.

--- QuickRadar/QRAppListPopover.m
+++ QuickRadar/QRAppListPopover.m
@@ -14,42 +14,30 @@
 
 @implementation QRAppListPopover
 
-// On versions of Mac OS X earlier than 10.10, we have to call viewWillAppear et. al. manually,
-// but on 10.10 and greater the view system will call it for us. Avoid calling it twice...
-- (BOOL) isMavericksOrOlder
-{
-	// 1265 is the same as NSAppKitVersionNumber10_9. The latter constant doesn't exist on Mavericks!
-	return (floor(NSAppKitVersionNumber) <= 1265);	
-}
-
 - (QRAppListViewController *) listViewController
 {
 	return (QRAppListViewController *)self.contentViewController;
 }
 
+// When building with SDKs older than 10.10, we have to call viewWillAppear et. al. manually,
+// but on 10.10 and greater the view system will call it for us. Avoid calling it twice...
+#if MAC_OS_X_VERSION_10_9 <= MAC_OS_X_VERSION_MAX_ALLOWED
 - (void)popoverWillShow:(NSNotification *)notification {
-	if ([self isMavericksOrOlder]) {
-		[[self listViewController] viewWillAppear];
-	}
+	[[self listViewController] viewWillAppear];
 }
 
 - (void)popoverDidShow:(NSNotification *)notification {
-	if ([self isMavericksOrOlder]) {
-		[[self listViewController] viewDidAppear];
-	}
+	[[self listViewController] viewDidAppear];
 }
 
 - (void)popoverWillClose:(NSNotification *)notification {
-	if ([self isMavericksOrOlder]) {
-		[[self listViewController] viewWillDisappear];
-	}
+	[[self listViewController] viewWillDisappear];
 }
 
 - (void)popoverDidClose:(NSNotification *)notification {
-	if ([self isMavericksOrOlder]) {
-		[[self listViewController] viewDidDisappear];
-	}
+	[[self listViewController] viewDidDisappear];
 }
+#endif
 
 - (void)selectedApp:(QRCachedRunningApplication *)app {
 	if ([self.appListDelegate respondsToSelector:@selector(appListPopover:selectedApp:)]) {
